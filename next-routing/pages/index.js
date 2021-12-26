12d113c6e3262382a22572c401231ac09b1e7a9b
baliswathi519 <baliswathi519@gmail.com>
2021-12-26 17:01:56 +0530

    Navigate Programatically

--- next-routing/pages/index.js
+++ next-routing/pages/index.js
@@ -1,6 +1,18 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
+import { useCallback } from "react";
 
 function Home() {
+    const router = useRouter();
+    const handleClick = useCallback(
+        () => {
+            console.log('placing your order');
+            router.push('/product');
+            //or
+            //router.replace('/product')
+        },
+        [],
+    )
     return <div>
         <h1>Home Page</h1>
         <Link href="/blog">
@@ -9,6 +21,7 @@ function Home() {
         <Link href="/product">
             <a>Go to Products</a>
         </Link>
+        <button onClick={handleClick}>Plcae order</button>
     </div>
 }
 export default Home
