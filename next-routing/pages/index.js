63bf3fe9ec7f4f61fc37b7ba8c756b893625580b
baliswathi519 <baliswathi519@gmail.com>
2021-12-26 16:55:28 +0530

    Link Component

--- next-routing/pages/index.js
+++ next-routing/pages/index.js
@@ -1,4 +1,14 @@
+import Link from "next/link"
+
 function Home() {
-    return <h1>Home Page</h1>
+    return <div>
+        <h1>Home Page</h1>
+        <Link href="/blog">
+            <a>Go to Blog</a>
+        </Link>
+        <Link href="/product">
+            <a>Go to Products</a>
+        </Link>
+    </div>
 }
 export default Home
--- next-routing/pages/product/index.js
+++ next-routing/pages/product/index.js
@@ -1,9 +1,23 @@
-function ProductList() {
+import Link from "next/link"
+
+function ProductList({ productId = 100 }) {
     return (
         <>
-            <h1>Product 1</h1>
-            <h1>Product 2</h1>
-            <h1>Product 3</h1>
+            <Link href="/">
+                <a>Go Home</a>
+            </Link>
+            <Link href="/product/1">
+                <a>Product 1</a>
+            </Link>
+            <Link href="/product/2">
+                <a>Product 2</a>
+            </Link>
+            <Link href="/product/3">
+                <a>Product 3</a>
+            </Link>
+            <Link href={`/product/${productId}`}>
+                <a>Product {productId}</a>
+            </Link>
         </>
     )
 }
